91259e49b637a5e5ba8a00b13a743d53c9beb41c
Tin Dev <buiductin123tg@gmail.com>
2026-09-01 11:05:32 +0700

    setup database enviroment

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$dbname = "painting_shop";
+$username = "root";
+$password = "";
+
+$conn = new PDO(
+    "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+    $username,
+    $password
+);
+
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
